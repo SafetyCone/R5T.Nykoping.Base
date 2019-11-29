44e94bbedac4e080b430f41f23afdfedfe766ab8
MinexAutomation <minex.automation@gmail.com>
2019-11-29 14:17:04 -0800

    Added Add(), Exists(), and HasEmailAddress() methods.

--- source/R5T.Nykoping.Base/Code/Services/Interfaces/IEmailEndpointRepository.cs
+++ source/R5T.Nykoping.Base/Code/Services/Interfaces/IEmailEndpointRepository.cs
@@ -7,6 +7,11 @@ namespace R5T.Nykoping
 {
     public interface IEmailEndpointRepository
     {
+        void Add(EndpointIdentity endpoint, string emailAddress);
+
+        bool Exists(EndpointIdentity endpoint);
+
+        bool HasEmailAddress(EndpointIdentity endpoint);
         void SetEmailAddress(EndpointIdentity endpoint, string emailAddress);
         string GetEmailAddress(EndpointIdentity endpoint);
     }
