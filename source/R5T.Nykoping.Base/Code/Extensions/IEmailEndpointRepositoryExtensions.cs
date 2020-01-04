3f9cb7cf1f5bb244537bd8a71f0d2969f47db945
MinexAutomation <minex.automation@gmail.com>
2020-01-04 11:24:28 -0800

    Added extensions for the IEmailEndpointRepository.

---
+++ source/R5T.Nykoping.Base/Code/Extensions/IEmailEndpointRepositoryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.Endalia;
+
+
+namespace R5T.Nykoping
+{
+    public static class IEmailEndpointRepositoryExtensions
+    {
+        public static string GetEmailAddress(this IEmailEndpointRepository repository, Guid identityValue)
+        {
+            var emailAddress = repository.GetEmailAddress(new EndpointIdentity(identityValue));
+            return emailAddress;
+        }
+    }
+}
